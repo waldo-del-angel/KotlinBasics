3681c2a9891bcf14655fc73ec7232eed0a35388a
Oswaldo Del Angel <oudelangel@gmail.com>
2022-04-30 20:37:52 -0500

    Read two strings from console

---
+++ Cheeseboard/read_data/Exercise01.kt
@@ -0,0 +1,13 @@
+/**
+ * Write a program that reads two strings and
+ * outputs them in the reverse order, each in a new line.
+ */
+fun main() {
+    // Read data
+    val a = readLine()!!
+    val b = readLine()!!
+
+    // Print values
+    println(b)
+    println(a)
+}
