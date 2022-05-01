f70432d9b90ff773a8652d9070c87dcdeacea062
Oswaldo Del Angel <oudelangel@gmail.com>
2022-04-30 21:24:47 -0500

    Repet a topic: type conversion

---
+++ Cheeseboard/type_conversion.kt/remember01.kt
@@ -0,0 +1,9 @@
+/**
+ * Let's write a lie detector program!
+ * The program should take a string as an input, convert it
+ * to a boolean value, and return it as the result.
+ */
+fun main() {
+    val isLie = readLine()!!
+    println(isLie.toBoolean())
+}
